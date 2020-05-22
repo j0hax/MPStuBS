7e31057c5425e9c420c56e8bd276c4b41ec64806
Johannes Arnold <joh.arnold@lab.sra.uni-hannover.de>
2020-05-22 14:39:02 +0200

    App1 Instanziiert

--- user/app1/appl.cc
+++ user/app1/appl.cc
@@ -1,15 +1,18 @@
 // vim: set noet ts=4 sw=4:
 
 #include "appl.h"
+#include "utils/spinlock.h"
 
 void Application::action() {
     kout << "Called " << __FUNCTION__ << "() in " << __FILE__ << endl;
 
-    // TODO: Implement a lock of some kind
-    
+    // Spinlock instanzieren
+    Spinlock l;
 
     // Endlosschleife
-    for(;;) {
-       
+    for(int i = 0;;i++) {
+        l.lock();
+        dout_CPU0 << i << endl;
+        l.unlock();
     }
 }
