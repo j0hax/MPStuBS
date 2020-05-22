fe434fe7649941f22a75f47e2a3fd6337f4ae945
Johannes Arnold <joh.arnold@lab.sra.uni-hannover.de>
2020-05-22 11:18:06 +0200

    Transfer

--- user/app1/appl.cc
+++ user/app1/appl.cc
@@ -1,7 +1,15 @@
 // vim: set noet ts=4 sw=4:
 
-#include "user/app1/appl.h"
+#include "appl.h"
 
 void Application::action() {
     kout << "Called " << __FUNCTION__ << "() in " << __FILE__ << endl;
+
+    // TODO: Implement a lock of some kind
+    
+
+    // Endlosschleife
+    for(;;) {
+       
+    }
 }
