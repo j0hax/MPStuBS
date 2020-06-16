8fb37aa575f8b5d1a903179ec0cbcee81a7548d3
Gerion Entrup <entrup@sra.uni-hannover.de>
2020-06-16 15:12:24 +0200

    Vorgabe Aufgabe 4

---
+++ machine/toc.cc
@@ -0,0 +1,9 @@
+// vim: set noet ts=4 sw=4:
+
+#include "machine/toc.h"
+
+// TOC_SETTLE: bereitet den Kontext der Koroutine fuer den ersten Aufruf vor.
+void toc_settle (struct toc *regs, void *tos, void (*kickoff)(Thread*), Thread *object)
+{
+}
+
